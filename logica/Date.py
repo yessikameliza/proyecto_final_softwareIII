9c7e29e2ec42fed5c4731dc5f69c3ac15dec0933
unknown <ymvelasquezr@uqvirtual.edu.co>
2018-09-25 09:23:10 -0500

    creacion de las clases block Date Hour y sede

---
+++ logica/Date.py
@@ -0,0 +1,5 @@
+class Date:
+    def __init__(self, id_Date, date, origin):
+        self.id_Date = id_Date
+        self.date = date
+        self.origin = origin
---
+++ logica/Hour.py
@@ -0,0 +1,5 @@
+class Hour:
+    def __init__(self, id_Hour, hourIni, hourFin):
+        self.id_Hour = id_Hour
+        self.hourIni = hourIni
+        self.hourFin = hourFin
