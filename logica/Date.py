164a65ca0fd3ae28bd5c7745787eb5b0ef48e526
unknown <ymvelasquezr@uqvirtual.edu.co>
2018-09-25 23:19:48 -0500

    creacion de la tabla admin

--- logica/Date.py
+++ logica/Date.py
@@ -5,7 +5,7 @@ from sqlite3.dbapi2 import Date
 class Date(object):
     date: Date
     origin: str
-    
+
     def __init__(self, date, origin):
         self.date = date
         self.origin = origin
