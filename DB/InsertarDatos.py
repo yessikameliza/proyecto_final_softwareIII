0778f8abcd12070b17a690423426c4f620c08b5f
unknown <ymvelasquezr@uqvirtual.edu.co>
2018-09-25 20:28:19 -0500

    creacion del metodo insertar docente desde persistencia a la base de datos

--- DB/InsertarDatos.py
+++ DB/InsertarDatos.py
@@ -1,10 +1,11 @@
 # enconding: utf-8
 # IMPORTANTE convertir el script a la codificación UTF-8
 import sqlite3
-from logica.Persistence import matter
+from logica.Matter import Matter
+from logica.Docent import Docent
 
 
-def insertarMatter():
+def insertMatter(matter: Matter):
     conexion = sqlite3.connect("DateBases.sqlite3")
 
     # Seleccionar el cursor para iniciar una consulta
@@ -47,6 +48,52 @@ def insertarMatter():
     else:
         print("la materia ya existe")
 
+    # Cerrar la consulta
+    consulta.close()
+
+    # Guardar los cambios en la base de datos
+    conexion.commit()
+
+    # Cerrar la conexión
+    conexion.close()
+
+
+def insertDocent(docent: Docent):
+    conexion = sqlite3.connect("DateBases.sqlite3")
+
+    # Seleccionar el cursor para iniciar una consulta
+    consulta = conexion.cursor()
+
+    print("**** Programa para insertar datos en bases de datos sqlite3 ****")
+    # TypeBlock = input("tipo de bloque: ")
+
+    name = docent.name
+    limitHours = docent.limitHours
+    contract = docent.contract
+    phone = docent.phone
+    identification = docent.identification
+    # Capturar excepciones para los números enteros y decimal
+    # Sólo números enteros
+    # Valor de los argumentos
+    argumentos = (name, limitHours, contract, phone, identification)
+    # argumentos2 = TypeBlock
+    # consulta SQL con argumentos ?, ?, ?, ?, ?
+    # sql2 = """INSERT INTO block (TypeBlock)
+    # VALUES (?)"""
+    sql = """INSERT INTO docent (name, limitHours, contract, phone, identification)
+    VALUES (?, ?, ?, ?, ?, ?)"""
+    sql2 = """SELECT * FROM docent WHERE identification = identification """
+    consulta.execute(sql2)
+
+    # Realizar la consulta
+    filas = consulta.fetchall()
+    if filas is None:
+        if consulta.execute(sql, argumentos):
+            print("Tabla creada con éxito")
+        else:
+            print("Ha ocurrido un error al crear la tabla")
+    else:
+        print("la materia ya existe")
 
     # Cerrar la consulta
     consulta.close()
--- logica/Persistence.py
+++ logica/Persistence.py
@@ -1,23 +1,27 @@
 from logica.Matter import Matter
 from logica.Docent import Docent
-from DB.InsertarDatos import insertarMatter
-matter = Matter()
-docent = Docent()
+from DB.InsertarDatos import insertMatter
+from DB.InsertarDatos import insertDocent
+
+
 
 
 def register_Matter(codigo, name, ubi_Semester, numCredit, codRequisite, numHoursSem):
+    matter = Matter()
     matter.codigo = codigo
     matter.name = name
     matter.ubi_Semester = ubi_Semester
     matter.numCredit = numCredit
     matter.codRequisite = codRequisite
     matter.numHoursSem = numHoursSem
-    insertarMatter()
+    insertMatter(matter)
 
 
 def register_Docent(name, limitHours, contract, phone, identification):
+    docent = Docent()
     docent.name = name
     docent.limitHours = limitHours
     docent.contract = contract
     docent.phone = phone
     docent.identification = identification
+    insertDocent(docent)
