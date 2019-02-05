161c0a32cd1b7dadb1aee6be704463a75f45e139
unknown <ymvelasquezr@uqvirtual.edu.co>
2019-02-04 23:00:53 -0500

    cambio

--- test/mymoodule.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-def sum(a, b):
-    for n in (a, b):
-        if not isinstance(n, int) and not isinstance(n, float):
-            raise TypeError
-    return a + b
