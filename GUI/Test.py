493675360378d94556ba798e021a95b4456a87a6
alexander leal <ealealr@uqvirtual.edu.co>
2019-02-11 15:48:13 -0500

    Pruebas unitarias, actualizar materia y docente

--- GUI/Test.py
+++ GUI/Test.py
@@ -1,8 +1,12 @@
 import unittest
 from logica.Persistence import register_matter
 from logica.Persistence import search_matter
-from logica.Persistence import delete_matter
 from logica.Persistence import register_docent
+from logica.Persistence import register_date
+from logica.Persistence import obtener_fechas_p
+from logica.Persistence import update_docent
+from logica.Persistence import search_docent
+from logica.Persistence import update_matter
 
 
 class Test(unittest.TestCase):
@@ -12,16 +16,40 @@ class Test(unittest.TestCase):
         a: str = search_matter("1233")[2]
         self.assertEqual(a, "caalculo 1")
 
-    def test_remove_matter(self):
-        print(search_matter("1233"))
-        delete_matter("1233")
-        print(search_matter("1233"))
-        a: str = search_matter("1233")
 
-        self.assertEquals(a[2], None)
 
     def test_register_docent(self):
         register_docent("melisa", "activo", 100, "catedratico", "1094970", "324234", "calculo", "Armenia")
+
+
+
+
+
+
+
+
+    def test_register_fechas(self):
+        register_date("11/02/2019", "13/04/2018", 1, "Primeras fechas" )
+        auxi = obtener_fechas_p("11/02/2019", "13/04/2018")
+        for aux in auxi:
+            aux2 = str((aux[2]))
+            print(aux2)
+            self.assertEqual(aux2, "13/04/2018")
+
+
+    def test_actualizar_docente(self):
+        register_docent("Alexander", "Activo", 300, "Ocasional", "3229491208", "1054565199","Estadistica", "Pereira")
+        update_docent("Alexander", "Inactivo", 300, "Ocasional", "3229491208", "1054565199", "Estadistica", "Pereira")
+        aux: str = search_docent("1054565199")[2]
+        self.assertEqual(aux, "Inactivo")
+
+
+
+    def test_actualizar_materia(self):
+        register_matter("1001", "Fundamentos en obras", "4", "3", "1234567", 240)
+        update_matter("1001", "Fundamentos en obras civiles", "5", "3", "1234567", 260)
+        aux: str = search_matter("1001")[2]
+        self.assertEqual(aux, "Fundamentos en obras civiles")
 
 
 if __name__ == "__main__":
